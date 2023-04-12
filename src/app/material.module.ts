ea9de5b5cdd26878e07fb43204187ea1a89fb5b3
Amakoye <charlesamakoye@gmail.com>
2023-04-12 10:10:38 +0300

    create the app structure

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
     declarations: [],
-    imports: [],
-    exports: [],
+    imports: [MatButtonModule, MatIconModule],
+    exports: [MatButtonModule, MatIconModule],
     providers: [],
 })
 
